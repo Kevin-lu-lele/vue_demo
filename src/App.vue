27b9ba86a240501e029987fd6e15996745e6cbf2
kevin <lulele.ok@163.com>
2020-09-23 12:39:38 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <router-view></router-view>
+      <!-- <FooterGuide v-show="$route.path!=='/login'"/> -->
+      <FooterGuide v-show="$route.meta.showFoot"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+   components: {
+       FooterGuide
+   }
+}
+</script>
+
+<style lang="less">
+  #app {
+    width: 100%;
+    height: 100%;
+    background: #f5f5f5;
+    // overflow: hidden;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * 入口
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index.js'
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router
+})
